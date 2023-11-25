342f8223ee4dca557516f87af97725b325ed035e
kamrannajafov <144939179+kamrannajafov@users.noreply.github.com>
2023-11-25 22:28:22 +0400

    Update User.java
    #2 commit.
    toString method was added.

--- User.java
+++ User.java
@@ -1,29 +1,39 @@
-
-
 public class User {
     private String username;
     private String password;
 
+
     public User(String username, String password){
     setUsername(username);
     setPassword(password);;
-    }   
+    }  
+
 
     public String getUsername(String username){
         return username;
     }
-    
+   
     public void setUsername(String username){
         this.username=username;
     }
 
+
     public String getPassword(String password){
         return password;
     }
+
 
     public void setPassword(String password){
         this.password=password;
     }
 
 
+    @Override
+    public String toString() {
+        return "User [username=" + username + ", password=" + password + "]";
+    }
+
+
 }
+
+
