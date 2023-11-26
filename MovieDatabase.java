223ae7eaa9505f146f709db48e4d46543fa94d15
sevda-m-16214 <smammadova16214@ada.edu.az>
2023-11-26 18:03:06 +0400

    adde movies to the database

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -4,19 +4,21 @@ import java.util.Iterator;
 public class MovieDatabase {
     public static void main(String[] args) {
         ArrayList<Movie> MovieDatabase = new ArrayList<Movie>();
-        MovieDatabase.add(new Movie("Movie1", "Director1", 2000, 123));
-        MovieDatabase.add(new Movie("Movie2", "Director2", 2013, 345));
-
-        printDatabase(MovieDatabase);
-
-        MovieDatabase.remove(1);
+        MovieDatabase.add(new Movie("Lilja 4ever", " Lukas Moodysson",
+                2002, 109));
+        MovieDatabase.add(new Movie("Girl Interrupted", "James Mangold",
+                1999, 127));
+        MovieDatabase.add(new Movie("The piano teacher", "Michael Haneke",
+                2001, 131));
+        MovieDatabase.add(new Movie("Manchester By The Sea", "Kenneth Lonergan",
+                2016, 137));
 
         printDatabase(MovieDatabase);
     }
 
     static <Movie>  void printDatabase(ArrayList<Movie> MovieDatabase){
         Iterator it  = MovieDatabase.iterator();
-        while (it.hasNext()) System.out.println(it.next());
+        while (it.hasNext()) System.out.println("----\n" + it.next());
         System.out.println("-----------------------");
     }
 
