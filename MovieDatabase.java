45745cb9795695856665d74da850fb7891c7e363
sevda-m-16214 <smammadova16214@ada.edu.az>
2023-11-28 16:49:32 +0400

    add new print method

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -8,17 +8,29 @@ public class MovieDatabase {
                 2002, 109));
         MovieDatabase.add(new Movie("Girl Interrupted", "James Mangold",
                 1999, 127));
-        MovieDatabase.add(new Movie("The piano teacher", "Michael Haneke",
+        MovieDatabase.add(new Movie("The Piano Teacher", "Michael Haneke",
                 2001, 131));
         MovieDatabase.add(new Movie("Manchester By The Sea", "Kenneth Lonergan",
                 2016, 137));
 
+
+        printMovie(MovieDatabase, 2);
+
         printDatabase(MovieDatabase);
     }
 
-    static <Movie>  void printDatabase(ArrayList<Movie> MovieDatabase){
+    static void printMovie(ArrayList<Movie> MovieDatabase, int index){ //prints one selected Movie from the ArrayLiat
+        System.out.println("----------------------------------------------\n" + MovieDatabase.get(index).toString()
+                + "\n----------------------------------------------");
+    }
+
+
+    static void printDatabase(ArrayList<Movie> MovieDatabase){ //prints all the Movies in the ArrayList
         Iterator it  = MovieDatabase.iterator();
-        while (it.hasNext()) System.out.println("----\n" + it.next());
+        while (it.hasNext()){
+            System.out.println("----\n" + it.next());
+        
+        }
         System.out.println("-----------------------");
     }
 
