2ad3109780499bcb471558e17795240c9ae7f417
javad <jjorujev@gmail.com>
2023-12-27 19:31:40 +0400

    Update MovieDatabaseApp.java
    added a browse movies option

--- MovieDatabaseApp.java
+++ MovieDatabaseApp.java
@@ -5,10 +5,10 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MovieDatabaseApp extends JFrame {
-    
-    private JTextField titleField, directorField, releaseYearField, runningTimeField;
+
     private JTextArea movieDisplayArea;
     private JComboBox<String> movieSelector;
+    private JPanel mainPanel;
     private ArrayList<Movie> movieDatabase;
 
     public MovieDatabaseApp() {
@@ -16,87 +16,91 @@ public class MovieDatabaseApp extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 400);
         setLayout(new BorderLayout());
-        JPanel entryPanel = new JPanel(new GridLayout(5, 2));
-        titleField = new JTextField();
-        directorField = new JTextField();
-        releaseYearField = new JTextField();
-        runningTimeField = new JTextField();
-        entryPanel.add(new JLabel("Title:"));
-        entryPanel.add(titleField);
-        entryPanel.add(new JLabel("Director:"));
-        entryPanel.add(directorField);
-        entryPanel.add(new JLabel("Release Year:"));
-        entryPanel.add(releaseYearField);
-        entryPanel.add(new JLabel("Running Time (minutes):"));
-        entryPanel.add(runningTimeField);
+        
+        getContentPane().setBackground(new Color(70, 70, 70));
+        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        headerPanel.setBackground(new Color(139, 164, 67));
+        JButton addMoviesButton = new JButton("Add Movies");
+        JButton browseMoviesButton = new JButton("Browse Movies");
 
-        JPanel buttonPanel = new JPanel();
-        JButton addButton = new JButton("Add Movie");
-        JButton displayButton = new JButton("Display Movies");
-        buttonPanel.add(addButton);
-        buttonPanel.add(displayButton);
+        mainPanel = new JPanel(new BorderLayout());
 
-        JPanel displayPanel = new JPanel(new BorderLayout());
-        movieDisplayArea = new JTextArea(10, 40);
-        movieDisplayArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(movieDisplayArea);
-        displayPanel.add(scrollPane, BorderLayout.CENTER);
+        addMoviesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearMainPanel();
+                addMoviesPanel();
+            }
+        });
 
-        movieSelector = new JComboBox<>();
-        displayPanel.add(movieSelector, BorderLayout.SOUTH);
+        browseMoviesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearMainPanel();
+                browseMoviesPanel();
+            }
+        });
 
-        add(entryPanel, BorderLayout.NORTH);
-        add(buttonPanel, BorderLayout.CENTER);
-        add(displayPanel, BorderLayout.SOUTH);
+        headerPanel.add(addMoviesButton);
+        headerPanel.add(browseMoviesButton);
+
+        add(headerPanel, BorderLayout.NORTH);
+        add(mainPanel, BorderLayout.CENTER);
 
         movieDatabase = new ArrayList<>();
-
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String title = titleField.getText();
-                String director = directorField.getText();
-                int releaseYear = Integer.parseInt(releaseYearField.getText());
-                int runningTime = Integer.parseInt(runningTimeField.getText());
-
-                Movie newMovie = new Movie(title, director, releaseYear, runningTime);
-                movieDatabase.add(newMovie);
-                titleField.setText("");
-                directorField.setText("");
-                releaseYearField.setText("");
-                runningTimeField.setText("");
-                populateMovieSelector();
-            }
-        });
-
-        displayButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayMovies();
-            }
-        });
-
-        movieSelector.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int selectedIndex = movieSelector.getSelectedIndex();
-                if (selectedIndex >= 0) {
-                    displaySelectedMovie(selectedIndex);
-                }
-            }
-        });
 
         setVisible(true);
     }
 
-    private void populateMovieSelector() {
-        movieSelector.removeAllItems();
-        for (Movie movie : movieDatabase) {
-            movieSelector.addItem(movie.getTitle());
-        }
+    private void addMoviesPanel() {
+        JPanel addMoviesPanel = new JPanel(new GridLayout(5, 2));
+        JTextField titleField = new JTextField();
+        JTextField directorField = new JTextField();
+        JTextField releaseYearField = new JTextField();
+        JTextField runningTimeField = new JTextField();
+        addMoviesPanel.add(new JLabel("Title:"));
+        addMoviesPanel.add(titleField);
+        addMoviesPanel.add(new JLabel("Director:"));
+        addMoviesPanel.add(directorField);
+        addMoviesPanel.add(new JLabel("Release Year:"));
+        addMoviesPanel.add(releaseYearField);
+        addMoviesPanel.add(new JLabel("Running Time (minutes):"));
+        addMoviesPanel.add(runningTimeField);
+
+        JButton addButton = new JButton("Add Movie");
+        addButton.setBackground(new Color(142, 75, 174)); // #8E4BAE in RGB
+       addButton.setOpaque(true);
+        
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addMovie(titleField.getText(), directorField.getText(), releaseYearField.getText(), runningTimeField.getText());
+                titleField.setText("");
+                directorField.setText("");
+                releaseYearField.setText("");
+                runningTimeField.setText("");
+            }
+        });
+
+        mainPanel.add(addMoviesPanel, BorderLayout.CENTER);
+        mainPanel.add(addButton, BorderLayout.SOUTH);
+        mainPanel.revalidate();
+        mainPanel.repaint();
     }
 
-    private void displayMovies() {
+    private void browseMoviesPanel() {
+        movieDisplayArea = new JTextArea(10, 40);
+        movieDisplayArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(movieDisplayArea);
+
+        populateMovieDisplay();
+
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+        mainPanel.revalidate();
+        mainPanel.repaint();
+    }
+
+    private void populateMovieDisplay() {
         StringBuilder displayText = new StringBuilder();
         for (Movie movie : movieDatabase) {
             displayText.append(movie.toString()).append("\n-----------------\n");
@@ -104,8 +108,25 @@ public class MovieDatabaseApp extends JFrame {
         movieDisplayArea.setText(displayText.toString());
     }
 
-    private void displaySelectedMovie(int index) {
-        movieDisplayArea.setText(movieDatabase.get(index).toString());
+    private void addMovie(String title, String director, String releaseYear, String runningTime) {
+        try {
+            int releaseYearInt = Integer.parseInt(releaseYear);
+            int runningTimeInt = Integer.parseInt(runningTime);
+            Movie newMovie = new Movie(title, director, releaseYearInt, runningTimeInt);
+            movieDatabase.add(newMovie);
+
+            if (mainPanel.getComponentCount() > 0) {
+                browseMoviesPanel(); // Refresh the browse movies panel
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Please enter valid release year and running time.");
+        }
+    }
+
+    private void clearMainPanel() {
+        mainPanel.removeAll();
+        mainPanel.revalidate();
+        mainPanel.repaint();
     }
 
     public static void main(String[] args) {
